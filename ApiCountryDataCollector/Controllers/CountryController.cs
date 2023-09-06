1e77dedd31c7444b5b7ae72be2e6de33e95bd479
jmorenorodriguez <jmorenorodriguez@trueblue.com>
2023-09-05 22:59:24 -0500

    UC#1 Create endpoint

--- ApiCountryDataCollector/Controllers/CountryController.cs
+++ ApiCountryDataCollector/Controllers/CountryController.cs
@@ -1,15 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
-namespace ApiCountryDataCollector.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class CountryController : ControllerBase
-{   
-    private readonly ILogger<CountryController> _logger;
-
-    public CountryController(ILogger<CountryController> logger)
+namespace RestCountriesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
     {
-        _logger = logger;
+        private readonly HttpClient _httpClient;
+
+        public CountriesController(IHttpClientFactory httpClientFactory)
+        {
+           _httpClient = httpClientFactory.CreateClient();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCountries([FromQuery] string param1, [FromQuery] string param2, [FromQuery] string param3, [FromQuery] string param4)
+        {
+            // Create the URL with parameters (you can customize this based on your requirements).
+            string apiUrl = "https://restcountries.com/v3.1/all";
+
+            // Make the HTTP request to the REST Countries API.
+            var response = await _httpClient.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the JSON response to a variable/object.
+                var json = await response.Content.ReadAsStringAsync();
+                var countriesData = JsonSerializer.Deserialize<object>(json);
+
+                return Ok(countriesData);
+            }
+
+            return BadRequest("Failed to retrieve data from the API.");
+        }
     }
 }
